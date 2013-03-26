4b63bfed4f76ffb5fa3d862c1a5e9d70d9ceafc1
Rohit <rohitsm@gmail.com>
2013-03-26 21:27:36 +0800

    Web crawler code from Udacity CS101 class

---
+++ Udacitycrawler.py
@@ -0,0 +1,64 @@
+def crawl_web(seed):
+    tocrawl = set([seed])  # this should be a set
+    crawled = set()      # this should be a set
+    graph = {}
+    index = {} 
+    while tocrawl: 
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            tocrawl.update(outlinks)
+            crawled.add(page)
+    return index, 
+
+
+
+
+
+def get_page(url):
+if url in cache:
+    return cache[url]
+else:
+    return None
+    
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+        
+def add_to_index(index, keyword, url):
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]
+
+def lookup(index, keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
+
+
